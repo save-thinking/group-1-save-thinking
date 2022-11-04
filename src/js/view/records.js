456bacbabd8a4d274d16dcb714ddb45f4f234f9a
Simran Barnwal <simranbarnwal@Simrans-MacBook-Air.local>
2022-11-03 21:17:27 -0700

    made changes to incorporate comments like better naming and using utility functions

--- src/js/view/records.js
+++ src/js/view/records.js
@@ -1,3 +1,5 @@
+import * as util from './utility.js'
+
 const addRecordModal = document.querySelector('#add-record-modal')
 const addRecordButton = document.querySelector('#add-record-btn')
 const navBarQuickAdd = document.querySelector('#add-record')
@@ -8,7 +10,7 @@ const toggleAddRecordModalVisibility = () => {
   addRecordModal.classList.toggle('hidden')
 }
 
-const recordsPlaceholder = document.querySelector('#record-list')
+const recordList = document.querySelector('#record-list')
 
 // the dummy record
 const testRecordData = [{
@@ -34,7 +36,7 @@ const testRecordData = [{
 
 // this function would be called every time the page is loaded
 document.addEventListener('DOMContentLoaded', () => {
-  createListWithInnerHTML(testRecordData)
+  testRecordData.map((record) => addRecordCard(record))
 })
 
 addRecordButton.onclick = (e) => {
@@ -52,10 +54,8 @@ recordModalAddBtn.onclick = (e) => {
   for (const pair of formData.entries()) {
     formJson[pair[0]] = pair[1]
   }
-  console.log(formJson)
   testRecordData.push(formJson)
-  // alert(JSON.stringify(formJson))
-  createListWithInnerHTML(testRecordData)
+  addRecordCard(formJson)
   toggleAddRecordModalVisibility()
 }
 
@@ -65,49 +65,32 @@ document.onkeyup = (e) => {
   }
 }
 
-const createListWithInnerHTML = (records) => {
-  // gereate the list of records HTML
-  const rows = records.map(record => {
-    // insert the currency sign after the negative sign
-    const currencySign = getCurrencySign(record.currency)
-    let amount = record.record_amount
-    if (record.record_amount.search('-') === 0) {
-      amount = amount.slice(0, 1) + currencySign + amount.slice(1)
-    } else {
-      amount = currencySign + amount
-    }
-
-    return `<li class="py-3 sm:py-4">
-    <div class="flex items-center space-x-4">
-      <div class="flex">
-        <div class="text-2xl rounded-full">🍕</div>
-      </div>
-      <div class="flex-1 min-w-0">
-        <p class="text-sm font-medium text-gray-900 truncate">${record.record_type}</p>
-        <p class="text-sm text-gray-500 truncate">${record.record_created_time}</p>
-      </div>
-      <div class="flex-1 min-w-0">
-        <p class="text-sm font-medium text-gray-900 truncate">
-          ${record.record_note}
-        </p>
-        <p class="text-sm text-gray-500 truncate">${record.record_source_account}</p>
-      </div>
-      <div class="inline-flex items-center text-base text-red-600">
-       ${amount}
-      </div>
-    </div>
-  </li>`
-  })
-  // console.log(rows)
-  const html = `<ul>${rows.join('')}</ul>`
-  // console.log(html)
-
-  // put the HTML into the web page
-  recordsPlaceholder.innerHTML = html
+const addRecordCard = (account) => {
+  recordList.prepend(recordCardComponent(account))
 }
 
-// helper function to return sign of currency
-const getCurrencySign = (currencyString) => {
-  if (currencyString === 'USD') { return '$' } else if (currencyString === 'EUR') { return '€' }
-  if (currencyString === 'INR') { return '₹' } else { return '' }
+const recordCardComponent = (record) => {
+  const recordCard = document.createElement('div')
+  recordCard.classList = 'py-3 sm:py-4'
+  recordCard.innerHTML = `
+<div class="flex items-center space-x-4">
+  <div class="flex">
+    <div class="text-2xl rounded-full">🍕</div>
+  </div>
+  <div class="flex-1 min-w-0">
+    <p class="text-sm font-medium text-gray-900 truncate">${record.record_type}</p>
+    <p class="text-sm text-gray-500 truncate">${record.record_created_time}</p>
+  </div>
+  <div class="flex-1 min-w-0">
+    <p class="text-sm font-medium text-gray-900 truncate">
+      ${record.record_note}
+    </p>
+    <p class="text-sm text-gray-500 truncate">${record.record_source_account}</p>
+  </div>
+  <div class="inline-flex items-center text-base text-red-600">
+   ${util.getBalanceWithCurrency(record.record_amount, record.currency)}
+  </div>
+</div>`
+  return recordCard
 }
+
