9dc9ffedabba1d6e0ee268a91cf55a7e37903077
Rohan Mishra <rohan.mishra1997@gmail.com>
2022-12-08 09:20:45 -0800

    Made Summary Dashboard Dynamic

--- src/js/view/dashboard.js
+++ src/js/view/dashboard.js
@@ -1,8 +1,15 @@
 /** View Service - Dashboard
  @module view/dashboard
  */
+import { Chart, registerables } from 'chart.js'
 import * as accountService from '../domain/account.js'
-import { recordModal, accountCardComponent } from './components.js'
+import * as summaryService from '../domain/summary.js'
+import {
+  recordModal,
+  accountCardComponent,
+  latestRecordCardComponent,
+  COLORS
+} from './components.js'
 const navBarQuickAdd = document.querySelector('#add-record')
 const addAccountModal = document.querySelector('#add-account-modal')
 const addAccountButton = document.querySelector('#add-account-btn')
@@ -12,6 +19,13 @@ const accountModalCancelButton = document.querySelector(
 const accountModalAddButton = document.querySelector('#add-account-modal-btn')
 const accountModalAddForm = document.querySelector('#add-account-form')
 const accountList = document.querySelector('#account-list')
+const cashflowCardBalance = document.querySelector('#cashflow-card-balance')
+const cashflowCardExpense = document.querySelector('#cashflow-card-expense')
+const cashflowCardIncome = document.querySelector('#cashflow-card-income')
+const cashflowChart = document.getElementById('cashflow-chart')
+const latestRecordsList = document.getElementById('recent-records')
+const donut = document.getElementById('expense-donut-chart')
+Chart.register(...registerables)
 
 // this function would be called every time the page is loaded
 document.addEventListener('DOMContentLoaded', () => {
@@ -21,6 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
       addAccountCard(account)
     })
   })
+  populateSummary()
 })
 
 /* Utilities */
@@ -29,6 +44,12 @@ const toggleAddAccountModalVisibility = () => {
 }
 const toggleAddRecordModalVisibility = () => {
   recordModal.classList.toggle('hidden')
+}
+
+const populateSummary = () => {
+  populateCashFlow()
+  populateLatestRecords()
+  populateExpenseByCategories()
 }
 
 /* Event Listeners */
@@ -67,4 +88,64 @@ document.onkeyup = (e) => {
 
 const addAccountCard = (account) => {
   accountList && accountList.prepend(accountCardComponent(account))
+}
+
+const populateCashFlow = async () => {
+  const cashflow = await summaryService.getCashFlow()
+  cashflowCardBalance.innerHTML = `$${cashflow.balance}`
+  cashflowCardExpense.innerHTML = `$${cashflow.expense}`
+  cashflowCardIncome.innerHTML = `$${cashflow.income}`
+  const cashflowData = {
+    labels: ['Income', 'Expense'],
+    datasets: [
+      {
+        label: 'Category',
+        data: [cashflow.income, cashflow.expense],
+        backgroundColor: ['rgb(34, 197, 94)', 'rgb(225, 29, 72)'],
+        borderWidth: 1,
+        barThickness: 20
+      }
+    ]
+  };
+  (() =>
+    new Chart(cashflowChart, {
+      type: 'bar',
+      data: cashflowData,
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    }))()
+}
+
+const populateLatestRecords = async () => {
+  let records = await summaryService.getLatestRecords()
+  records = records.map((record) => latestRecordCardComponent(record))
+  records.forEach((record) => latestRecordsList.appendChild(record))
+}
+
+const populateExpenseByCategories = async () => {
+  const answer = await summaryService.getExpenseByCategories()
+  const data = {
+    labels: Object.keys(answer),
+    datasets: [
+      {
+        label: 'Amount',
+        data: Object.values(answer),
+        backgroundColor: COLORS.slice(0, Object.keys(answer).length),
+        hoverOffset: 4
+      }
+    ]
+  };
+  (() =>
+    new Chart(donut, {
+      type: 'doughnut',
+      data,
+      options: {
+        responsive: false
+      }
+    }))()
 }
